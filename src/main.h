f55970b727c64086c5f671cb895a9723d467239b
Laszlo Csirmaz <csirmaz@degas.ceu.hu>
2025-10-04 15:06:38 +0200

    Initial version

---
+++ src/main.h
@@ -0,0 +1,22 @@
+/** main.h -- the main program **/
+
+/***********************************************************************
+ * This code is part of MAXE, a helper program for maximum entropy method.
+ *
+ * Copyright (C) 2025 Laszlo Csirmaz, https://github.com/lcsirmaz/MAXE
+ *
+ * This program is free, open-source software. You may redistribute it
+ * and/or modify under the terms of the GNU General Public License (GPL).
+ *
+ * There is ABSOLUTELY NO WARRANTY, use at your own risk.
+ ***********************************************************************/
+
+/***********************************************************************
+* int dobreak, dodump;
+*      increased by one every time BREAK_SIGNAL or DUMP_SIGNAL is received
+*/
+
+extern volatile int dobreak, dodump;
+
+/* EOF */
+
---
+++ src/outer.h
@@ -0,0 +1,33 @@
+/** outer.h  -- bounds and default values **/
+
+/***********************************************************************
+ * This code is part of MAXE, a helper program for maximum entropy method.
+ *
+ * Copyright (C) 2025 Laszlo Csirmaz, https://github.com/lcsirmaz/MAXE
+ *
+ * This program is free, open-source software. You may redistribute it
+ * and/or modify under the terms of the GNU General Public License (GPL).
+ *
+ * There is ABSOLUTELY NO WARRANTY, use at your own risk.
+ ***********************************************************************/
+
+/***********************************************************************
+* the outer approximation algorithm
+*
+* int outer(void)
+*    execute the outer approximation algorithm for the prepared data.
+*    Return value:
+*      0   program terminated normally
+*      1   data error before the algorithm started
+*      2   problem unbounded
+*      3   problem unfeasible
+*      4   error during algorithm execution
+*      5   interrupted, postprocessing terminated normally
+*      6   interrupted, error in postprocessing
+*      7   interrupted, postprocessing aborted
+*/
+
+int outer(void);
+
+/* EOF */
+
